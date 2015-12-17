e7882c40214a9972b260d2b9a534ec14ae67999c
Jarl Holta <Jarlholta@gmail.com>
2015-12-17 23:39:46 +0100

    Booltests at least compile.
    Idk if there is any issues with this, but should not be from my
    understanding of the problem.

--- lpvartypes_ord.pas
+++ lpvartypes_ord.pas
@@ -633,7 +633,10 @@ end;
 
 function TLapeType_Enum.EvalAsSubType(Op: EOperator; Right: TLapeType): Boolean;
 const
-  BoolOperators = BinaryOperators + EnumOperators - [op_cmp_Equal, op_cmp_NotEqual];
+  BoolOperators = BinaryOperators + EnumOperators - [op_cmp_GreaterThan, op_cmp_GreaterThanOrEqual,
+                                                     op_cmp_LessThan, op_cmp_LessThanOrEqual,
+                                                     op_cmp_Equal, op_cmp_NotEqual,
+                                                     op_AND, op_OR, op_XOR];
 begin
   if (Right = nil) or (not Right.IsOrdinal()) then
     Result := False
