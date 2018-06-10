1b955e5864cf2d3503ec6b2cfd383830837f0dc4
NielsAD <niels.a.d@gmail.com>
2018-06-10 10:41:28 +0200

    Fix error when using break in wrong place (resolves #114)

--- lptree.pas
+++ lptree.pas
@@ -2674,7 +2674,7 @@ begin
     Node := Node.Parent;
   end;
 
-  if (BreakCount < BreakDepth) then
+  if (FParams.Count = 1) and (BreakCount < BreakDepth) then
     LapeException(lpeOutOfTypeRange, FParams[0].DocPos)
   else if (FoundNode = nil) then
     LapeException(lpeCannotBreak, DocPos)
