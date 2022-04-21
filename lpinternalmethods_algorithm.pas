f0f1cec33d2ed8e8541c512f3b648c4c75e58693
ollydev <olly.cain@outlook.com>
2022-04-21 20:48:00 +0100

    Add ArrayMean & ArrayVariance

--- lpinternalmethods_algorithm.pas
+++ lpinternalmethods_algorithm.pas
@@ -44,6 +44,18 @@ type
     function Compile(var Offset: Integer): TResVar; override;
   end;
 
+  TLapeTree_InternalMethod_ArrayMean = class(TLapeTree_InternalMethod)
+  public
+    function resType: TLapeType; override;
+    function Compile(var Offset: Integer): TResVar; override;
+  end;
+
+  TLapeTree_InternalMethod_ArrayVariance = class(TLapeTree_InternalMethod)
+  public
+    function resType: TLapeType; override;
+    function Compile(var Offset: Integer): TResVar; override;
+  end;
+
 const
   _LapeArrayMode: lpString =
     'function _ArrayMode(p: Pointer; ElSize, Len: SizeInt;'         + LineEnding +
@@ -589,6 +601,203 @@ begin
 
     Compile(Offset);
   finally
+    CounterVar.Spill(1);
+
+    Free();
+  end;
+
+  Result.isConstant := True;
+end;
+
+function TLapeTree_InternalMethod_ArrayMean.resType: TLapeType;
+var
+  Typ: TLapeType;
+begin
+  if (FResType = nil) then
+    if (FParams.Count = 1) and (not isEmpty(FParams[0])) then
+    begin
+      Typ := FParams[0].resType();
+
+      if (Typ is TLapeType_DynArray) then
+      begin
+        if (TLapeType_DynArray(Typ).PType.BaseType in LapeRealTypes+LapeIntegerTypes) then
+          FResType := FCompiler.getBaseType(ltDouble)
+        else
+          FResType := TLapeType_DynArray(Typ).PType;
+      end;
+    end;
+
+  Result := inherited;
+end;
+
+function TLapeTree_InternalMethod_ArrayMean.Compile(var Offset: Integer): TResVar;
+var
+  ArrayVar, LengthVar: TResVar;
+begin
+  Result := NullResVar;
+  Dest := NullResVar;
+
+  if (FParams.Count <> 1) then
+    LapeExceptionFmt(lpeWrongNumberParams, [1], DocPos);
+  if (resType() = nil) or (not FParams[0].CompileToTempVar(Offset, ArrayVar, 1)) then
+    LapeException(lpeExpectedArray, DocPos);
+
+  RequireOperators(FCompiler, [op_Plus], TLapeType_DynArray(ArrayVar.VarType).PType, DocPos);
+  RequireOperators(FCompiler, [op_Divide], TLapeType_DynArray(ArrayVar.VarType).PType, FCompiler.getBaseType(ltDouble), DocPos);
+
+  Result := _ResVar.New(FCompiler.getTempVar(resType()));
+
+  FCompiler.VarToDefault(Result, Offset);
+
+  with TLapeTree_InternalMethod_Length.Create(Self) do
+  try
+    addParam(TLapeTree_ResVar.Create(ArrayVar.IncLock(), Self));
+
+    LengthVar := FoldConstants(False).Compile(Offset);
+  finally
+    Free();
+  end;
+
+  with TLapeTree_If.Create(Self) do
+  try
+    Condition := TLapeTree_ResVar.Create(LengthVar.IncLock(), Self);
+    Body := TLapeTree_Operator.Create(op_Assign, Self);
+    with TLapeTree_Operator(Body) do
+    begin
+      Left := TLapeTree_ResVar.Create(Result.IncLock(), Self);
+
+      Right := TLapeTree_Operator.Create(op_Divide, Self);
+      with TLapeTree_Operator(Right) do
+      begin
+        Left := TLapeTree_InternalMethod_ArraySum.Create(Self);
+        with TLapeTree_InternalMethod_ArraySum(Left) do
+          addParam(TLapeTree_ResVar.Create(ArrayVar.IncLock(), Self));
+        Right := TLapeTree_ResVar.Create(LengthVar.IncLock(), Self);
+      end;
+    end;
+
+    Compile(Offset);
+  finally
+    LengthVar.Spill(1);
+
+    Free();
+  end;
+
+  Result.isConstant := True;
+end;
+
+function TLapeTree_InternalMethod_ArrayVariance.resType: TLapeType;
+var
+  Typ: TLapeType;
+begin
+  if (FResType = nil) then
+    if (FParams.Count = 1) and (not isEmpty(FParams[0])) then
+    begin
+      Typ := FParams[0].resType();
+
+      if (Typ is TLapeType_DynArray) then
+      begin
+        if (TLapeType_DynArray(Typ).PType.BaseType in LapeRealTypes+LapeIntegerTypes) then
+          FResType := FCompiler.getBaseType(ltDouble)
+        else
+          FResType := TLapeType_DynArray(Typ).PType;
+      end;
+    end;
+
+  Result := inherited;
+end;
+
+function TLapeTree_InternalMethod_ArrayVariance.Compile(var Offset: Integer): TResVar;
+var
+  LengthVar, ArrayVar, CounterVar, SquareVar, MeanVar: TResVar;
+  ArrayType: TLapeType;
+  Minus: TLapeTree_Operator;
+begin
+  Result := NullResVar;
+  Dest := NullResVar;
+
+  if (FParams.Count <> 1) then
+    LapeExceptionFmt(lpeWrongNumberParams, [1], DocPos);
+
+  ArrayType := resType();
+  if (ArrayType = nil) or (not FParams[0].CompileToTempVar(Offset, ArrayVar, 1)) then
+    LapeException(lpeExpectedArray, DocPos);
+
+  RequireOperators(FCompiler, [op_cmp_LessThan, op_cmp_GreaterThan, op_Minus], ArrayType, DocPos);
+
+  Result := _ResVar.New(FCompiler.getTempVar(ArrayType));
+  CounterVar := _ResVar.New(FCompiler.getTempVar(ArrayType));
+  SquareVar := _ResVar.New(FCompiler.getTempVar(ArrayType));
+  MeanVar := _ResVar.New(FCompiler.getTempVar(ArrayType));
+
+  FCompiler.VarToDefault(Result, Offset);
+  FCompiler.VarToDefault(SquareVar, Offset);
+
+  with TLapeTree_InternalMethod_Length.Create(Self) do
+  try
+    addParam(TLapeTree_ResVar.Create(ArrayVar.IncLock(), Self));
+
+    LengthVar := FoldConstants(False).Compile(Offset);
+  finally
+    Free();
+  end;
+
+  with TLapeTree_If.Create(Self) do
+  try
+    Condition := TLapeTree_ResVar.Create(LengthVar.IncLock(), Self);
+    Body := TLapeTree_StatementList.Create(Self);
+    with TLapeTree_StatementList(Body) do
+    begin
+      addStatement(TLapeTree_Operator.Create(op_Assign, Self));
+      with TLapeTree_Operator(Statements[0]) do
+      begin
+        Left := TLapeTree_ResVar.Create(MeanVar.IncLock(), Self);
+        Right := TLapeTree_InternalMethod_ArrayMean.Create(Self);
+        with TLapeTree_InternalMethod_ArrayMean(Right) do
+          addParam(TLapeTree_ResVar.Create(ArrayVar.IncLock(), Self));
+      end;
+
+      addStatement(TLapeTree_For.Create(Self));
+      with TLapeTree_For(Statements[1]) do
+      begin
+        LoopType := loopOver;
+
+        Counter := TLapeTree_ResVar.Create(CounterVar.IncLock(), Self);
+        Limit := TLapeTree_ResVar.Create(ArrayVar.IncLock(), Self);
+        Body := TLapeTree_Operator.Create(op_AssignPlus, Self);
+        with TLapeTree_Operator(Body) do
+        begin
+          Left := TLapeTree_ResVar.Create(SquareVar.IncLock(), Self);
+
+          Minus := TLapeTree_Operator.Create(op_Minus, Self);
+          with TLapeTree_Operator(Minus) do
+          begin
+            Left := TLapeTree_ResVar.Create(CounterVar.IncLock(), Self);
+            Right := TLapeTree_ResVar.Create(MeanVar.IncLock(), Self);
+          end;
+
+          Right := TLapeTree_Invoke.Create('Sqr', Self);
+          with TLapeTree_Invoke(Right) do
+            addParam(Minus);
+        end;
+      end;
+
+      addStatement(TLapeTree_Operator.Create(op_Assign, Self));
+      with TLapeTree_Operator(Statements[2]) do
+      begin
+        Left := TLapeTree_ResVar.Create(Result.IncLock(), Self);
+        Right := TLapeTree_Operator.Create(op_Divide, Self);
+        with TLapeTree_Operator(Right) do
+        begin
+          Left := TLapeTree_ResVar.Create(SquareVar.IncLock(), Self);
+          Right := TLapeTree_ResVar.Create(LengthVar.IncLock(), Self);
+        end;
+      end;
+    end;
+
+    Compile(Offset);
+  finally
+    LengthVar.Spill(1);
     CounterVar.Spill(1);
 
     Free();
