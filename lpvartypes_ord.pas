df463fc01a9137961c7ac9d67c77d22d5737f5ca
ollydev <olly.cain@outlook.com>
2022-04-28 17:23:21 +0100

    Fix #180

--- lpvartypes_ord.pas
+++ lpvartypes_ord.pas
@@ -645,7 +645,7 @@ begin
   if (Str <> '') and (AnsiChar(Str[1]) in ['-', '0'..'9']) then
     Result := NewGlobalVar(StrToInt64(string(Str)), AName, ADocPos)
   else
-    Result := NewGlobalVar(FMemberMap.IndexOf(string(Str)), AName, ADocPos);
+    Result := NewGlobalVar(FRange.Lo + FMemberMap.IndexOf(string(Str)), AName, ADocPos);
 end;
 
 function TLapeType_Enum.EvalAsSubType(Op: EOperator; Right: TLapeType): Boolean;
